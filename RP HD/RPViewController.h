f44f2d8de972884c8205c2240a11c8cefe36f6e0
Giacomo Tufano <gt@iltofa.it>
2012-03-15 19:38:53 +0100

    Almost working. Still a bug on image loading...

--- RP HD/RPViewController.h
+++ RP HD/RPViewController.h
@@ -7,6 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#include <QuartzCore/QuartzCore.h>
+#import "AudioStreamer.h"
+
+#define kRPURL24K @"http://stream-tx1.radioparadise.com:8022"
+#define kRPURL64K @"http://207.200.96.231:8004"
+#define kRPURL128K @"http://scfire-mtc-aa03.stream.aol.com:80/stream/1048"
+
+#define kRPm3u24K @"http://www.radioparadise.com/musiclinks/rp_24aac.m3u"
+#define kRPm3u64K @"http://www.radioparadise.com/musiclinks/rp_64aac.m3u"
+#define kRPm3u128K @"http://www.radioparadise.com/musiclinks/rp_128.m3u"
+
+#define kHDImageURLURL @"http://radioparadise.com/readtxt.php"
 
 @interface RPViewController : UIViewController
 
@@ -14,10 +26,15 @@
 @property (weak, nonatomic) IBOutlet UIImageView *coverImage;
 @property (weak, nonatomic) IBOutlet UIButton *playOrStopButton;
 @property (weak, nonatomic) IBOutlet UIView *volumeViewContainer;
+@property (weak, nonatomic) IBOutlet UIWebView *webImage;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
+@property (strong, nonatomic) AudioStreamer *theStreamer;
+@property (strong, nonatomic) NSOperationQueue *imageLoadQueue;
+@property (strong, nonatomic) NSTimer *theTimer;
+@property (copy, nonatomic) NSString *theURL;
 
 - (IBAction)playOrStop:(id)sender;
 - (IBAction)bitrateChanged:(id)sender;
-- (IBAction)refreshImage:(id)sender;
 
 @end
--- RP HD/RPViewController.m
+++ RP HD/RPViewController.m
@@ -14,27 +14,267 @@
 @end
 
 @implementation RPViewController
-@synthesize metadataInfo;
-@synthesize coverImage;
-@synthesize playOrStopButton;
-@synthesize volumeViewContainer;
-@synthesize spinner;
+
+@synthesize metadataInfo = _metadataInfo;
+@synthesize coverImage = _coverImage;
+@synthesize playOrStopButton = _playOrStopButton;
+@synthesize volumeViewContainer = _volumeViewContainer;
+@synthesize webImage = _webImage;
+@synthesize spinner = _spinner;
+@synthesize theStreamer = _theStreamer;
+@synthesize imageLoadQueue = _imageLoadQueue;
+@synthesize theURL = _theURL;
+@synthesize theTimer = _theTimer;
+
+#pragma mark -
+#pragma mark HD images loading
+-(void)loadNewImage:(NSTimer *)timer
+{
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:kHDImageURLURL]];
+    [NSURLConnection sendAsynchronousRequest:req queue:self.imageLoadQueue completionHandler:^(NSURLResponse *res, NSData *data, NSError *err)
+     {
+         NSLog(@"HD image url received %@ ", (data) ? @"successfully." : @"with errors.");
+         if(data)
+         {
+             NSString *imageUrl = [[NSString stringWithUTF8String:[data bytes]] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+             if(imageUrl)
+             {
+                 NSLog(@"Now loading real image url: <%@>", imageUrl);
+                 [self.webImage loadHTMLString:[NSString stringWithFormat:@"<body bgcolor=#000000 leftmargin=\"0\" rightmargin=\"0\" topmargin=\"0\" bottommargin=\"0\"><img style=\"width:100%%\" src=\"%@\"/></body>", imageUrl] baseURL:nil];
+             }
+             else {
+                 NSLog(@"Got an invalid URL");
+             }
+         }
+     }];
+}
+
+#pragma mark -
+#pragma mark AudioStream Notifications management
+
+-(void)metadataNotificationReceived:(NSNotification *)note
+{
+    // Parse metadata...
+    NSString *metadata = self.theStreamer.metaDataString;
+    
+    NSLog(@"Raw metadata: %@", metadata);
+    NSLog(@" Stream type: %@", self.theStreamer.streamContentType);
+	NSArray *listItems = [metadata componentsSeparatedByString:@";"];
+    NSRange range;
+    for (NSString *item in listItems) {
+        NSLog(@"item: %@", item);
+        // Look for title
+        range = [item rangeOfString:@"StreamTitle="];
+        if(range.location != NSNotFound)
+        {
+            NSString *temp = [[item substringFromIndex:range.length] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"'\""]];
+            NSLog(@"Song name: %@", temp);
+            self.metadataInfo.text = temp;
+        }
+        // Look for URL
+        range = [item rangeOfString:@"StreamUrl="];
+        if(range.location != NSNotFound)
+        {
+            NSString *temp = [item substringFromIndex:range.length];
+            temp = [temp stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"'\""]];
+            NSLog(@"URL: <%@>", temp);
+            [self.imageLoadQueue cancelAllOperations];
+            NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:temp]];
+            [NSURLConnection sendAsynchronousRequest:req queue:self.imageLoadQueue completionHandler:^(NSURLResponse *res, NSData *data, NSError *err)
+             {
+                 NSLog(@"coverReceived %@ ", (data) ? @"successfully." : @"with errors.");
+                 if(data)
+                     self.coverImage.image = [UIImage imageWithData:data];
+                 else
+                     self.coverImage.image = [UIImage imageNamed:@"Radio-160"];
+             }];
+            // Now load the big image...
+            [self loadNewImage:nil];
+        }
+    }
+}
+
+-(void)errorNotificationReceived:(NSNotification *)note
+{
+	NSLog(@"Stream Error.");
+    [self stopPressed:nil];
+}
+
+-(void)streamRedirected:(NSNotification *)note
+{
+	NSLog(@"Stream Redirected\nOld: <%@>\nNew: %@", self.theURL, [self.theStreamer.url absoluteString]);
+    self.theURL = [self.theStreamer.url absoluteString];
+    [self stopPressed:nil];
+    [self playPressed:nil];
+}
+
+-(void) startSpinner
+{
+    [self.spinner startAnimating];
+    self.metadataInfo.text = @"";
+}
+
+-(void)stopSpinner:(NSNotification *)note
+{
+    [self.spinner stopAnimating];
+    if(!note)
+    {
+        self.metadataInfo.text = @"";
+        self.coverImage.image = [UIImage imageNamed:@"Radio-160"];
+    }
+}
+
+#pragma mark -
+#pragma mark Actions
+
+- (void)playPressed:(id)sender 
+{
+    self.theStreamer = [[AudioStreamer alloc] initWithURL:[NSURL URLWithString:self.theURL]];
+    [self startSpinner];
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-stop"] forState:UIControlStateNormal];
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-stop"] forState:UIControlStateHighlighted];
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-stop"] forState:UIControlStateSelected];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(metadataNotificationReceived:) name:kStreamHasMetadata object:nil]; 
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(errorNotificationReceived:) name:kStreamIsInError object:nil]; 
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(stopSpinner:) name:kStreamConnected object:nil]; 
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(streamRedirected:) name:kStreamIsRedirected object:nil]; 
+    self.theTimer = [NSTimer scheduledTimerWithTimeInterval:30.0 target:self selector:@selector(loadNewImage:) userInfo:nil repeats:YES];
+    self.webImage.hidden = NO;
+    [self.theStreamer start];
+}
+
+- (void)stopPressed:(id)sender 
+{
+    [self.theStreamer stop];
+    [self.theTimer invalidate];
+    self.theTimer = nil;
+    [self stopSpinner:nil];
+    self.webImage.hidden = YES;
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-play"] forState:UIControlStateNormal];
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-play"] forState:UIControlStateHighlighted];
+    [self.playOrStopButton setImage:[UIImage imageNamed:@"button-play"] forState:UIControlStateSelected];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kStreamHasMetadata object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kStreamIsInError object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kStreamConnected object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kStreamIsRedirected object:nil];
+    self.theStreamer = nil;
+}
+
+- (IBAction)playOrStop:(id)sender 
+{
+    if(self.theStreamer.isPlaying)
+        [self stopPressed:nil];
+    else
+        [self playPressed:nil];
+}
+
+- (IBAction)bitrateChanged:(id)sender 
+{
+    switch (((UISegmentedControl *)sender).selectedSegmentIndex) 
+    {
+        case 0:
+            self.theURL = kRPURL24K;
+            break;
+        case 1:
+            self.theURL = kRPURL64K;
+            break;
+        case 2:
+            self.theURL = kRPURL128K;
+            break;
+        default:
+            break;
+    }
+    // If needed, restart the stream
+    if(self.theStreamer.isPlaying)
+    {
+        [self stopPressed:nil];
+        [self playPressed:nil];
+    }
+}
+
+#pragma mark -
+#pragma mark LoadUnload
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+    // reset text
+    self.metadataInfo.text = @"";
+    self.theURL = kRPURL64K;
+    [self.webImage loadHTMLString:@"<body bgcolor=#000000></body>" baseURL:nil];
+    self.webImage.layer.cornerRadius = 10.0;
+    self.webImage.clipsToBounds = YES;
+    self.coverImage.layer.cornerRadius = 5.0;
+    self.coverImage.clipsToBounds = YES;
+    // Add the volume (fake it on simulator)
+    self.volumeViewContainer.backgroundColor = [UIColor clearColor];
+    if (!TARGET_IPHONE_SIMULATOR)
+    {
+        MPVolumeView *myVolumeView = [[MPVolumeView alloc] initWithFrame:self.volumeViewContainer.bounds];
+        [self.volumeViewContainer addSubview: myVolumeView];
+        myVolumeView = nil;
+    }
+    else
+    {
+        UISlider *myVolumeView = [[UISlider alloc] initWithFrame:self.volumeViewContainer.bounds];
+        myVolumeView.value = 0.5;
+        [self.volumeViewContainer addSubview: myVolumeView];
+        myVolumeView = nil;
+    }
+    self.imageLoadQueue = [[NSOperationQueue alloc] init];
 }
 
 - (void)viewDidUnload
 {
     [self setMetadataInfo:nil];
+    [self setSpinner:nil];
+    [self setVolumeViewContainer:nil];
+    [self.imageLoadQueue cancelAllOperations];
+    [self setImageLoadQueue:nil];
     [self setCoverImage:nil];
     [self setPlayOrStopButton:nil];
-    [self setVolumeViewContainer:nil];
-    [self setSpinner:nil];
+    [self setWebImage:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
+}
+
+#pragma mark -
+#pragma mark Multimedia Remote Control
+- (void)viewDidAppear:(BOOL)animated 
+{
+    [super viewDidAppear:animated];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    [self becomeFirstResponder];
+}
+
+- (void)viewWillDisappear:(BOOL)animated 
+{
+    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
+    [self resignFirstResponder];
+    [super viewWillDisappear:animated];
+}
+
+- (void) remoteControlReceivedWithEvent: (UIEvent *) receivedEvent 
+{
+    if (receivedEvent.type == UIEventTypeRemoteControl) {
+        switch (receivedEvent.subtype) 
+        {
+            case UIEventSubtypeRemoteControlTogglePlayPause:
+                [self playOrStop: nil];
+                break;
+            case UIEventSubtypeRemoteControlPreviousTrack:
+                break;
+            case UIEventSubtypeRemoteControlNextTrack:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (BOOL) canBecomeFirstResponder 
+{
+    return YES;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -42,12 +282,4 @@
     return YES;
 }
 
-- (IBAction)playOrStop:(id)sender {
-}
-
-- (IBAction)bitrateChanged:(id)sender {
-}
-
-- (IBAction)refreshImage:(id)sender {
-}
 @end
