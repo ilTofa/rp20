d7099960c28d852a814cc00c6621cf4a39c22d3c
Giacomo Tufano <gt@iltofa.it>
2012-07-16 17:36:30 +0200

    First login view

---
+++ RP HD/RPLoginController.h
@@ -0,0 +1,13 @@
+//
+//  RPLoginController.h
+//  RP HD
+//
+//  Created by Giacomo Tufano on 16/07/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPLoginController : UIViewController
+
+@end
---
+++ RP HD/RPLoginController.m
@@ -0,0 +1,43 @@
+//
+//  RPLoginController.m
+//  RP HD
+//
+//  Created by Giacomo Tufano on 16/07/12.
+//
+//
+
+#import "RPLoginController.h"
+
+@interface RPLoginController ()
+
+@end
+
+@implementation RPLoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
