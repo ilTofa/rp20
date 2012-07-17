eb5da7e838841854e94a761853a7d1a80de618cf
Giacomo Tufano <gt@iltofa.it>
2012-07-17 10:44:22 +0200

    First implementation of login

--- RP HD/RPLoginController.h
+++ RP HD/RPLoginController.h
@@ -15,6 +15,7 @@
 @property(strong, nonatomic) RPViewController *parent;
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UILabel *formHeader;
 
 - (IBAction)startPSD:(id)sender;
 - (IBAction)cancel:(id)sender;
--- RP HD/RPLoginController.m
+++ RP HD/RPLoginController.m
@@ -9,6 +9,7 @@
 #import "RPLoginController.h"
 
 #import "RPViewController.h"
+#import "STKeychain/STKeychain.h"
 
 @interface RPLoginController ()
 
@@ -19,6 +20,7 @@
 @synthesize parent = _parent;
 @synthesize usernameField = _usernameField;
 @synthesize passwordField = _passwordField;
+@synthesize formHeader = _formHeader;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -32,6 +34,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // Load saved...
+    // + (NSString *)getPasswordForUsername:(NSString *)username andServiceName:(NSString *)serviceName error:(NSError **)error;
+    NSError *err;
+    NSString *userName = [[NSUserDefaults standardUserDefaults] stringForKey:@"userName"];
+    if(userName)
+    {
+        self.usernameField.text = userName;
+        NSString *password = [STKeychain getPasswordForUsername:userName andServiceName:@"RP" error:&err];
+        if(password)
+            self.passwordField.text = password;
+    }
     [self.usernameField becomeFirstResponder];
 }
 
@@ -49,21 +62,49 @@
 - (void)viewDidUnload {
     [self setUsernameField:nil];
     [self setPasswordField:nil];
+    [self setFormHeader:nil];
     [super viewDidUnload];
 }
 
 - (IBAction)startPSD:(id)sender
 {
-    
+    // Try to login to RP
+    NSString *temp = [NSString stringWithFormat:@"http://www.radioparadise.com/ajax_login.php?username=%@&passwd=%@", self.usernameField.text, self.passwordField.text];
+    DLog(@"Logging in with <%@>", temp);
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:temp]];
+    [NSURLConnection sendAsynchronousRequest:req queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *res, NSData *data, NSError *err)
+     {
+         // Login is successful if data is not nil and data is not "invalid login"
+         NSString *retValue = nil;
+         if(data)
+             retValue = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+         BOOL invalidLogin = [retValue rangeOfString:@"invalid "].location != NSNotFound;
+         if(retValue && !invalidLogin)
+         { // If login successful, save info and send information to parent
+             NSError *err;
+             [[NSUserDefaults standardUserDefaults] setObject:self.usernameField.text forKey:@"userName"];
+             [STKeychain storeUsername:self.usernameField.text andPassword:self.passwordField.text forServiceName:@"RP" updateExisting:YES error:&err];
+         }
+         else
+         { // If not, notify the user (on main thread)
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 self.formHeader.text = @"Invalid login, please retry";
+                 self.formHeader.textColor = [UIColor redColor];
+             });
+         }
+     }];
 }
 
 - (IBAction)cancel:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
 - (IBAction)createNew:(id)sender
 {
-    
+    // Send user to RP mobile registering form <http://www.radioparadise.com/m-content.php?name=Members&file=register>
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.radioparadise.com/m-content.php?name=Members&file=register"]];
+    [self cancel:nil];
 }
 
 @end
