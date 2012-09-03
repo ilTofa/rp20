5662c2e7c1c956285987080155c081337a12b441
Giacomo Tufano <gt@iltofa.it>
2012-09-03 17:26:32 +0200

    Starting iCloud song implementation.

---
+++ RP HD/iCloud/SongsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongsViewController.h
+//  RP HD
+//
+//  Created by Giacomo Tufano on 03/09/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongsViewController : UITableViewController
+
+@end
